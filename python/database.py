1875750b55ba1313fb8e7c8b0b0da4c703b4ddfe
Garrett Brown <garbearucla@gmail.com>
2013-04-04 06:15:28 -0700

    Add unit tests to database.py

--- python/database.py
+++ python/database.py
@@ -1,13 +1,14 @@
 # Unofficial Windows binaries for Python packages can be found here:
 # http://www.lfd.uci.edu/~gohlke/pythonlibs/
 import MySQLdb
+import unittest
 
 db_name = 'game_shack'
 
 def CreateTables():
     pass
 
-def GetTables():
+def getTables():
     """
     Untested
     """
@@ -19,13 +20,13 @@ def GetTables():
     )
     return cur.fetchall()
 
-def GetIndices():
+def getIndices():
     """
     Untested
     """
     global cur
     indices = list()
-    for table in GetTables():
+    for table in getTables():
         # DISTINCT, because MySQL considers a UNIQUE INDEX across two columns
         # to be two indices, whereas SQLite only considers it a single one
         cur.execute(
@@ -40,11 +41,29 @@ def main():
     global db, cur
     db = MySQLdb.connect(host="localhost", user="root", passwd="xbmc", db=db_name)
     cur = db.cursor()
-
     CreateTables()
+
+
+class TestDatabase(unittest.TestCase):
+    def setUp(self):
+        global db, cur
+        if db == None:
+            db = MySQLdb.connect(host="localhost", user="root", passwd="xbmc", db=db_name)
+        if cur == None:
+            cur = db.cursor()
+
+    def test_get_tables(self):
+        tables = getTables()
+        self.assertEqual(len(tables), 0)
+
+    def test_get_game_list(self):
+        indices = getIndices()
+        self.assertEqual(len(indices), 0)
+
 
 db = None
 cur = None
 
 if __name__ == "__main__":
-    main()
+    #main()
+    unittest.main()
