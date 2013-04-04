bf04e4d5298fb87fa4f73d74874e9426a51e7ca6
Garrett Brown <garbearucla@gmail.com>
2013-04-04 06:45:42 -0700

    Added initial database structure and fixed entire database script

--- python/database.py
+++ python/database.py
@@ -6,7 +6,38 @@ import unittest
 db_name = 'game_shack'
 
 def CreateTables():
-    pass
+    global cur
+    cur.execute(
+        "CREATE TABLE gamefiles (" + \
+            "id INTEGER UNSIGNED AUTO_INCREMENT PRIMARY KEY, " + \
+            "filename VARCHAR(128), " + \
+            "site VARCHAR(10), " + \
+            "platform VARCHAR(40), " + \
+            "idProperty INTEGER, " + \
+            "created DATETIME DEFAULT NULL" + \
+        ")"
+    )
+    cur.execute(
+        "CREATE TABLE properties (" + \
+            "id INTEGER UNSIGNED AUTO_INCREMENT PRIMARY KEY, " + \
+            "code VARCHAR(16), " + \
+            "title VARCHAR(32), " + \
+            "publisher VARCHAR(32)" + \
+        ")"
+    )
+    cur.execute(
+        "CREATE TABLE usernames (" + \
+            "id INTEGER UNSIGNED AUTO_INCREMENT PRIMARY KEY, " + \
+            "username VARCHAR(32)" + \
+        ")"
+    )
+    cur.execute(
+        "CREATE TABLE gamefiles_link_usernames (" + \
+            "id INTEGER UNSIGNED AUTO_INCREMENT PRIMARY KEY, " + \
+            "username VARCHAR(32)" + \
+        ")"
+    )
+
 
 def getTables():
     """
@@ -14,11 +45,11 @@ def getTables():
     """
     global cur
     cur.execute(
-        "SELECT table_name " +
-        "FROM information_schema.TABLES " +
+        "SELECT table_name " + \
+        "FROM information_schema.TABLES " + \
         "WHERE table_schema='%s'" % db_name
     )
-    return cur.fetchall()
+    return [table[0] for table in cur.fetchall()]
 
 def getIndices():
     """
@@ -30,18 +61,19 @@ def getIndices():
         # DISTINCT, because MySQL considers a UNIQUE INDEX across two columns
         # to be two indices, whereas SQLite only considers it a single one
         cur.execute(
-            "SELECT DISTINCT index_name " +
-            "FROM information_schema.STATISTICS " +
+            "SELECT DISTINCT index_name " + \
+            "FROM information_schema.STATISTICS " + \
             "WHERE table_schema='%s' and table_name='%s'" % (db_name, table)
         )
-        indices.extend(index for index in cur.fetchall() if index != "PRIMARY")
+        indices.extend(index[0] for index in cur.fetchall() if index[0] != "PRIMARY")
     return indices
 
 def main():
     global db, cur
     db = MySQLdb.connect(host="localhost", user="root", passwd="xbmc", db=db_name)
     cur = db.cursor()
-    CreateTables()
+    if len(getTables()) == 0:
+        CreateTables()
 
 
 class TestDatabase(unittest.TestCase):
@@ -54,9 +86,13 @@ class TestDatabase(unittest.TestCase):
 
     def test_get_tables(self):
         tables = getTables()
-        self.assertEqual(len(tables), 0)
+        self.assertEqual(len(tables), 4)
+        self.assertTrue("gamefiles" in tables)
+        self.assertTrue("properties" in tables)
+        self.assertTrue("usernames" in tables)
+        self.assertTrue("gamefiles_link_usernames" in tables)
 
-    def test_get_game_list(self):
+    def test_get_indices(self):
         indices = getIndices()
         self.assertEqual(len(indices), 0)
 
@@ -65,5 +101,5 @@ db = None
 cur = None
 
 if __name__ == "__main__":
-    #main()
+    main()
     unittest.main()
